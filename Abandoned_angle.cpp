07142d813d92ce973ba4250f667b1dacab37e240
mRGinix <86766633+mRGinix@users.noreply.github.com>
2021-07-01 10:45:52 +0300

    Add files via upload

---
+++ Abandoned_angle.cpp
@@ -0,0 +1,25 @@
+﻿// Abondoned body at an angle  a = arcsin(gT/2V), Cin user T and V.
+//
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	double T, V, param, result;
+    const double Pi = asin(1.0), g = 9.8;
+	cout << "Enters numbers for T and V \n"; cin >> V >> T;
+	cout << endl;
+
+	if (1 < fabs(g * T / (2 * V)))
+		cout << "Incorrect" << endl;
+	else {
+		param = asin(g * T / (2 * V));
+		cout << "Angle = " << param * (90 / Pi) << endl;
+	}
+	system("PAUSE");
+	return 0;
+
+}
+
